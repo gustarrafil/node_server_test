beaf45ae8a334eb7fa34b45b2c3186f52bdaa1c7
Gustavo Tarrafil Sousa Pereira <gustavotarrafil@gmail.com>
2021-05-30 19:35:29 -0300

    adicionando componentes de transacao

--- index.js
+++ index.js
@@ -8,21 +8,34 @@ app.use(cors());
 // app.listen(3000);
 
 
-app.get('/', function(req, res){res.send('Hello world')});
+app.get('/', function(req, res){res.send('pagina inicial')});
 
 /*
   Servidor propriamente dito
 */
 
 const notes = [
-    {id: 0, title: "Vinícius Nonato Rodrigues", description : "Aluno da Unicamp feliz"},
-    {id: 1, title: "Carolina da Silva Sancho", description : "Aluna da Unicamp não tão feliz"}
+    {
+        id: 0, 
+        title: "Vinícius Nonato Rodrigues", 
+        description : "Aluno da Unicamp feliz"
+    },
+    {
+        id: 1, 
+        title: "Carolina da Silva Sancho", 
+        description : "Aluna da Unicamp não tão feliz"
+    }
 ]
+const transactions = []
 
 const endpoint = "/notes";
+const endpointTransaction = "/transaction";
 
 app.get(endpoint, function(req, res){
     res.send(notes.filter(Boolean));
+});
+app.get(endpointTransaction, function(req, res){
+    res.send(transactions.filter(Boolean));
 });
 
 app.get(`${endpoint}/:id`, function(req, res){
@@ -35,7 +48,28 @@ app.get(`${endpoint}/:id`, function(req, res){
         res.send(note);
     }   
 });
+app.get(`${endpointTransaction}/:transactionId`, function(req, res){
+    const transactionId = req.params.transactionId;
+    const transaction = transactions[transactionId];
 
+    if (!transaction){
+        res.send("{}");
+    } else {
+        res.send(transaction);
+    }   
+});
+
+app.post(endpointTransaction, (req, res) => {
+    const transaction = {
+        id : transactions.length,
+        title : req.body["title"],
+        description : req.body["description"]
+    };
+    transactions.push(transaction);
+    res.send("1");
+
+    notify();
+});
 app.post(endpoint, (req, res) => {
     const note = {
         id : notes.length,
@@ -48,6 +82,19 @@ app.post(endpoint, (req, res) => {
     notify();
 });
 
+app.put(`${endpointTransaction}/:transactionId`, (req, res) =>{
+    const transactionId = parseInt(req.params.transactionId);
+    const transaction = {
+        id : transactionId,
+        title : req.body["title"],
+        description : req.body["description"]
+    };
+
+    transactions[transactionId] = transaction;
+    res.send("1");
+
+    notify();
+});
 app.put(`${endpoint}/:id`, (req, res) =>{
     const id = parseInt(req.params.id);
     const note = {
@@ -62,6 +109,14 @@ app.put(`${endpoint}/:id`, (req, res) =>{
     notify();
 });
 
+app.delete(`${endpointTransaction}/:transactionId`, (req, res) => {
+    const transactionId = req.params.transactionId;
+    delete transactions[transactionId];
+    res.send("1");
+
+    // Notificar todos
+    notify();
+});
 app.delete(`${endpoint}/:id`, (req, res) => {
     const id = req.params.id;
     delete notes[id];
